42c7c4a8fba7ff394ff3aadd86e87b318d743eb2
Cristian Venegas <cristian.venegas@materoceano.com>
2022-12-19 17:07:40 -0300

    fix : index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const port = 3000;
 
-app.listen(3000, console.log("servidor funcionando"))
 
-app.length
+app.listen(port, console.log(`Servidor funcionado en el puerto : ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello Word")
+})
